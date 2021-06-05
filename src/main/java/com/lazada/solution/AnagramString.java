a401353acca30a2dc1acaf729d12abb858e1e93d
kunalboolchandani <kunalboolchandani@gmail.com>
2021-06-05 14:09:53 +0800

    first checkin

---
+++ src/main/java/com/lazada/solution/AnagramString.java
@@ -0,0 +1,41 @@
+package com.lazada.solution;
+
+/*
+        Q1. Please check if two Strings are "Anagram",
+        Definition of anagram: If you rearrange the order of the letters / characters in two strings,
+        you can get the same string: e.g: cinema, iceman.
+*/
+
+public class AnagramString {
+
+    public boolean areTwoStringsAnagram(String textA, String textB) {
+        if (textA == null && textB == null) {
+            return false;
+        }
+
+        if (textA == null || textB == null) {
+            return false;
+        }
+
+        if (textA.length() != textB.length()) {
+            return false;
+        }
+
+        int[] checkerArray = new int[256];
+        for (char a : textA.toCharArray()) {
+            checkerArray[a]++;
+        }
+
+        for (char b : textB.toCharArray()) {
+            checkerArray[b]--;
+        }
+
+        for (int netValue : checkerArray) {
+            if (netValue != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/com/lazada/solution/PathInGraph.java
@@ -0,0 +1,91 @@
+package com.lazada.solution;
+
+/*
+        Q3. Please design the data structures for a very large social network
+        like Tictok, Facebook, etc?
+        Please design an algorithm to show the path or link-chain between two people
+        (e.g., You –> Jim –> Lily –> David –> Me).
+*/
+
+import java.util.*;
+
+public class PathInGraph {
+
+    public static class Graph {
+        private final Map<String, Set<String>> adjacencyList = new HashMap<>();
+
+        public void addEdge(String node1, String node2) {
+            adjacencyList.putIfAbsent(node1, new LinkedHashSet<>());
+            adjacencyList.get(node1).add(node2);
+
+            adjacencyList.putIfAbsent(node2, new LinkedHashSet<>());
+            adjacencyList.get(node2).add(node1);
+        }
+
+        public List<String> getNeighbors(String node) {
+            adjacencyList.putIfAbsent(node, new LinkedHashSet<>());
+            return new LinkedList<>(adjacencyList.get(node));
+        }
+    }
+
+    public class Path {
+
+        private String start, end;
+        private List<List<String>> allPaths;
+
+        public Path(String start, String end) {
+            this.start = start;
+            this.end = end;
+            allPaths = new LinkedList<>();
+        }
+
+        public void add(List<String> validPath) {
+            allPaths.add(validPath);
+        }
+
+        public List<List<String>> getAllPaths() {
+            return allPaths;
+        }
+
+        public List<String> getMinimumPath(){
+            return allPaths.stream().min(Comparator.comparing(List::size)).orElseGet(LinkedList::new);
+        }
+    }
+
+
+    private String node1, node2;
+    private Path result;
+
+    public Path getPaths(Graph graph, String node1, String node2) {
+        this.node1 = node1;
+        this.node2 = node2;
+        this.result = new Path(node1, node2);
+        LinkedList<String> visited = new LinkedList<>();
+        visited.add(this.node1);
+        dfs(graph,visited);
+        return result;
+    }
+
+    private void dfs(Graph graph, LinkedList<String> visited) {
+
+        List<String> nodes = graph.getNeighbors(visited.getLast());
+
+        for (String node : nodes) {
+            if (node2.equals(node) && !visited.contains(node)) {
+                visited.add(node);
+                this.result.add(new LinkedList<>(visited));
+                visited.removeLast();
+                break;
+            }
+        }
+
+        for (String node : nodes) {
+            if (!visited.contains(node) && !node.equals(node2)) {
+                visited.addLast(node);
+                dfs(graph, visited);
+                visited.removeLast();
+            }
+        }
+    }
+
+}
